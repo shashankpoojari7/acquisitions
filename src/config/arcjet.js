f1d23ae3c8f397433f424ee777e03ade0a4907d0
Shashank Poojari <shashankpoojari00@gmail.com>
2026-01-13 19:49:28 +0530

    Secure our API with Arcjet

---
+++ src/config/arcjet.js
@@ -0,0 +1,17 @@
+import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+        "CATEGORY:PREVIEW",
+      ],
+    }),
+  ],
+});
+
+export default aj;
